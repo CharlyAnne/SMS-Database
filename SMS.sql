44c6761bf548e979ad974452cdac3a31c02a5be0
Charlian Imoisili <icharlieanne@gmail.com>
2025-06-20 14:00:33 +0100

    Update data structure

--- SMS.sql
+++ SMS.sql
@@ -1,27 +1,9 @@
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
---
 -- Host: localhost    Database: students_mgt_system
--- ------------------------------------------------------
 -- Server version	8.0.42
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
+---------------------------------------------------------
 -- Table structure for table `courses`
---
-
 DROP TABLE IF EXISTS `courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `courses` (
   `CourseID` int NOT NULL AUTO_INCREMENT,
   `CourseName` varchar(100) NOT NULL,
@@ -29,30 +11,18 @@ CREATE TABLE `courses` (
   PRIMARY KEY (`CourseID`),
   KEY `DepartmentID` (`DepartmentID`),
   CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)
 
---
 -- Table structure for table `departments`
---
-
 DROP TABLE IF EXISTS `departments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `departments` (
   `DepartmentID` int NOT NULL AUTO_INCREMENT,
   `DepartmentName` varchar(100) NOT NULL,
   PRIMARY KEY (`DepartmentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)
 
---
 -- Table structure for table `enrollments`
---
-
 DROP TABLE IF EXISTS `enrollments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `enrollments` (
   `EnrollmentID` int NOT NULL AUTO_INCREMENT,
   `StudentID` int DEFAULT NULL,
@@ -63,16 +33,10 @@ CREATE TABLE `enrollments` (
   KEY `CourseID` (`CourseID`),
   CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`),
   CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`CourseID`) REFERENCES `courses` (`CourseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) 
 
---
 -- Table structure for table `instructors`
---
-
 DROP TABLE IF EXISTS `instructors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `instructors` (
   `InstructorID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(100) NOT NULL,
@@ -83,16 +47,10 @@ CREATE TABLE `instructors` (
   KEY `CourseID` (`CourseID`),
   CONSTRAINT `instructors_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`),
   CONSTRAINT `instructors_ibfk_2` FOREIGN KEY (`CourseID`) REFERENCES `courses` (`CourseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)
 
---
 -- Table structure for table `students`
---
-
 DROP TABLE IF EXISTS `students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `students` (
   `StudentID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(100) NOT NULL,
@@ -102,20 +60,4 @@ CREATE TABLE `students` (
   PRIMARY KEY (`StudentID`),
   KEY `DepartmentID` (`DepartmentID`),
   CONSTRAINT `students_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping routines for database 'students_mgt_system'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-06-20 13:53:39
+)
