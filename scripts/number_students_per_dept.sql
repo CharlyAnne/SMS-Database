87b81598ec73e42bd48b0f49e053adb14270a156
Charly <icharlieanne@gmail.com>
2025-06-25 19:17:54 +0100

    no of students per dept

--- scripts/number_students_per_dept.sql
+++ scripts/number_students_per_dept.sql
@@ -1,8 +1,8 @@
 # Total number of students per department across all courses
 SELECT departments.DepartmentID, COUNT(DISTINCT enrollments.StudentID) AS TotalStudents
 FROM enrollments
-JOIN students
+JOIN students   #Joining enrollment table plus students tanle plus departments table
 ON enrollments.StudentID = students.StudentID
 JOIN departments 
 ON students.DepartmentID = departments.DepartmentID
-GROUP BY departments.DepartmentID
+GROUP BY departments.DepartmentID # group record by departments
